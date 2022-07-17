cf4a953e029857bb854d918eeb1adfbd628ad667
MatheusGorga <matheusgobar@gmail.com>
2022-07-17 02:14:02 -0300

    Formulários - Resetando formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -29,7 +29,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormDate = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormDate>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormDate>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -39,6 +39,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormDate) {
     console.log(data)
+    reset()
   }
 
   const task = watch('task')
