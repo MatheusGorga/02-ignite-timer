78e1a3fe396b8e0a9120faa70cf4cf125322a115
MatheusGorga <matheusgobar@gmail.com>
2022-07-17 01:56:28 -0300

    Formulários - Validando formulários

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -10,8 +13,18 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O intervalo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O intervalo precisa ser de no máximo 60 minutos'),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
@@ -40,7 +53,6 @@ export function Home() {
 
           <label htmlFor="minutesAmount">Durante</label>
           <MinutesAmountInput
-            min={0}
             max={60}
             step={5}
             placeholder="00"
